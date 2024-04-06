aaf6b15888c256e69bcb639bb0330c2bab2c3c5b
Myron Holmes <1dequanh@gmail.com>
2024-04-05 20:58:50 -0400

    login page customized

--- src/main/java/com/myprojects/customlogindemo/config/SecurityConfig.java
+++ src/main/java/com/myprojects/customlogindemo/config/SecurityConfig.java
@@ -12,8 +12,13 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-                .formLogin(form->form.loginPage("/login").permitAll())
-                .authorizeHttpRequests(auth->auth.anyRequest().authenticated())
+                .formLogin(form->form
+                        .loginPage("/login")
+                        .permitAll())
+                .authorizeHttpRequests(auth->auth
+                        .requestMatchers("/images/**").permitAll()
+                        .anyRequest()
+                        .authenticated())
                 .build();
     }
 }
